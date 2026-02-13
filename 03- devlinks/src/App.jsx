367c3116b575ce0aad42fd9feaa6e4abf86f1a67
AnnaJuMagaC <naju.magac@gmail.com>
2026-02-13 16:50:38 -0300

    feat: adicionar modo claro e componente de switch para alternar temas

--- 03- devlinks/src/App.jsx
+++ 03- devlinks/src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Link from "./components/Link/Link";
 import LinkSocial from "./components/LinkSocial/LinkSocial";
 import Perfil from "./components/Perfil/Perfil";
 import Rodape from "./components/Rodape/Rodape";
+import Switch from "./components/Switch/Switch";
 
 function App() {
+  const[isLight,setIsLight] = useState(true);
+
+  const troca = () => {
+    setIsLight (!isLight);   
+  };
+
   return (
-    <div id="App">
+    <div id="App" className={isLight ? "light" : ""}>
       <Perfil fotoPerfil={"https:placehold.co/100"}>Anna Maga</Perfil>
 
-      <div className="switch">botão switch </div>
+      <Switch troca={troca} isLight={isLight} />
 
       <div className="Link">
         <ul>
@@ -20,7 +28,7 @@ function App() {
         </ul>
       </div>
 
-      <div className="linksSocial">
+      <div id="LinksSocial">
         <LinkSocial url={"https://github.com/"} icon={"logo-github"} />
         <LinkSocial
           url={"https://www.instagram.com/"}
