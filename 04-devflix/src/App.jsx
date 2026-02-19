8420b49436d34eb2cb8fdec4d39809ce7a8bb120
AnnaJuMagaC <naju.magac@gmail.com>
2026-02-19 13:59:01 -0300

    feat: atualizar estilos do tema e modificar o componente Rodape para incluir link dinâmico

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
         <img src={lupa} alt="Botão de ação para pesquisa!" />
       </div>
 
-      <Rodape>Najulha</Rodape>
+      <Rodape link={"https://github.com/AnnaJuMagaC"}>Najulha</Rodape>
     </div>
     
   );
