a54a6c7a01b4df7782d37f6d62cd32e08c97927a
AnnaJuMagaC <naju.magac@gmail.com>
2026-02-19 11:21:05 -0300

    feat: implementar componente Rodape e integrar ao App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,13 +1,27 @@
-
-import "./App.css"
+import "./App.css";
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
   return (
-    <div>
-      App
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logo da plataforma de streaming Netflix com o nome em destaque, em letras vermelhas vibrantes sobre fundo escuro, 
+      representando entretenimento online."
+      />
+
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <img src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+
+      <Rodape>Najulha</Rodape>
     </div>
-  )
-}
+    
+  );
+};
 
-export default App
-
+export default App;
